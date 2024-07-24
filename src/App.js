e9069bd2a91adbd5b03c499e4ae7fade44f5bc57
Darrell Cox <darrellcox@MacBook-Pro.lan>
2024-07-24 21:20:23 +0100

    Fixed footer overlapping text

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import News from './components/News';
 import Events from './components/Events';
-import Challenge from './components/Challenge';
+// import Challenge from './components/Challenge';
 import Sponsors from './components/Sponsors';
-import Elevens from './components/Elevens';
-import Leagues from './components/Leagues';
-import TeamCopr from './components/TeamCopr';
-import TeamGwyr from './components/TeamGwyr';
-import LeagueCopr from './components/LeagueCopr';
-import LeagueGwyr from './components/LeagueGwyr';
-import TeamOfTheWeek from './components/TeamOfTheWeek';
+// import Elevens from './components/Elevens';
+// import Leagues from './components/Leagues';
+// import TeamCopr from './components/TeamCopr';
+// import TeamGwyr from './components/TeamGwyr';
+// import LeagueCopr from './components/LeagueCopr';
+// import LeagueGwyr from './components/LeagueGwyr';
+// import TeamOfTheWeek from './components/TeamOfTheWeek';
 
 
 // Gwyr League
@@ -51,47 +51,49 @@ function App() {
 
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/totw" element={<TeamOfTheWeek />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/events/:key" element={<Event />} />
-        <Route path="/challenge" element={<Challenge />} />
-        <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/elevens" element={<Elevens />} />
-        <Route path="/elevens/gwyr" element={<TeamGwyr />} />
-        <Route path="/elevens/copr" element={<TeamCopr />} />
-        <Route path="/leagues" element={<Leagues />} />
-        <Route path="/leagues/gwyr" element={<LeagueGwyr />} />
-        <Route path="/leagues/copr" element={<LeagueCopr />} />
-        <Route path="/player/:id" element={<PlayerProfile />} />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/totw" element={<TeamOfTheWeek />} /> */}
+          <Route path="/news" element={<News />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/:key" element={<Event />} />
+          {/* <Route path="/challenge" element={<Challenge />} /> */}
+          <Route path="/sponsors" element={<Sponsors />} />
+          {/* <Route path="/elevens" element={<Elevens />} /> */}
+          {/* <Route path="/elevens/gwyr" element={<TeamGwyr />} /> */}
+          {/* <Route path="/elevens/copr" element={<TeamCopr />} /> */}
+          {/* <Route path="/leagues" element={<Leagues />} /> */}
+          {/* <Route path="/leagues/gwyr" element={<LeagueGwyr />} /> */}
+          {/* <Route path="/leagues/copr" element={<LeagueCopr />} /> */}
+          <Route path="/player/:id" element={<PlayerProfile />} />
 
-        {/* Gwyr League */}
-        <Route path="/leagues/team/GwyrBlack" element={<GwyrBlack />} />
-        <Route path="/leagues/team/GwyrBlue" element={<GwyrBlue />} />
-        <Route path="/leagues/team/GwyrGreen" element={<GwyrGreen />} />
-        <Route path="/leagues/team/GwyrOrange" element={<GwyrOrange />} />
-        <Route path="/leagues/team/GwyrPurple" element={<GwyrPurple />} />
-        <Route path="/leagues/team/GwyrRed" element={<GwyrRed />} />
-        <Route path="/leagues/team/GwyrWhite" element={<GwyrWhite />} />
-        <Route path="/leagues/team/GwyrYellow" element={<GwyrYellow />} />
+          {/* Gwyr League */}
+          <Route path="/leagues/team/GwyrBlack" element={<GwyrBlack />} />
+          <Route path="/leagues/team/GwyrBlue" element={<GwyrBlue />} />
+          <Route path="/leagues/team/GwyrGreen" element={<GwyrGreen />} />
+          <Route path="/leagues/team/GwyrOrange" element={<GwyrOrange />} />
+          <Route path="/leagues/team/GwyrPurple" element={<GwyrPurple />} />
+          <Route path="/leagues/team/GwyrRed" element={<GwyrRed />} />
+          <Route path="/leagues/team/GwyrWhite" element={<GwyrWhite />} />
+          <Route path="/leagues/team/GwyrYellow" element={<GwyrYellow />} />
 
 
-        {/* Copr League */}
-        <Route path="/leagues/team/CoprBlack" element={<CoprBlack />} />
-        <Route path="/leagues/team/CoprBlue" element={<CoprBlue />} />
-        <Route path="/leagues/team/CoprGreen" element={<CoprGreen />} />
-        <Route path="/leagues/team/CoprOrange" element={<CoprOrange />} />
-        <Route path="/leagues/team/CoprPurple" element={<CoprPurple />} />
-        <Route path="/leagues/team/CoprRed" element={<CoprRed />} />
-        <Route path="/leagues/team/CoprWhite" element={<CoprWhite />} />
-        <Route path="/leagues/team/CoprYellow" element={<CoprYellow />} />
+          {/* Copr League */}
+          <Route path="/leagues/team/CoprBlack" element={<CoprBlack />} />
+          <Route path="/leagues/team/CoprBlue" element={<CoprBlue />} />
+          <Route path="/leagues/team/CoprGreen" element={<CoprGreen />} />
+          <Route path="/leagues/team/CoprOrange" element={<CoprOrange />} />
+          <Route path="/leagues/team/CoprPurple" element={<CoprPurple />} />
+          <Route path="/leagues/team/CoprRed" element={<CoprRed />} />
+          <Route path="/leagues/team/CoprWhite" element={<CoprWhite />} />
+          <Route path="/leagues/team/CoprYellow" element={<CoprYellow />} />
 
-        {/* Route to display custom 404 page */}
-        <Route path="*" element={<NotFound />} />
+          {/* Route to display custom 404 page */}
+          <Route path="*" element={<NotFound />} />
 
-      </Routes>
+        </Routes>
+      </div>
       <Footer />
     </div>
 
