4ab3b643d98fc020682dfc2956b013f4b7d191d9
Darrell Cox <darrellcox@MacBook-Pro.lan>
2024-07-25 21:25:48 +0100

    Update delete script and add first run of player input

--- scripts/delete_db_data.py
+++ scripts/delete_db_data.py
@@ -1,3 +1,22 @@
+import json
+import boto3
+import os
+import uuid  # Module for generating unique IDs
+
+from boto3.dynamodb.conditions import Key
+
+# Load AWS credentials and configure AWS SDK
+session = boto3.Session(
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    region_name="eu-west-2"
+)
+
+
+dynamodb = session.resource('dynamodb')
+players_table = dynamodb.Table('Players')
+
+
 # Function to delete all items from DynamoDB table
 def delete_all_items(table):
     scan = table.scan()
@@ -8,3 +27,6 @@ def delete_all_items(table):
                     'id': each['id']
                 }
             )
+
+# Call the function to delete all items
+delete_all_items(players_table)
